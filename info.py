c99be6f36681d3f0862285497ef48e32b7eb6c20
Vis10n7 <134475497+Vis10n7@users.noreply.github.com>
2023-05-24 11:20:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ if SELF_DELETE == "True":
 
     # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 HOW TO DOWNLOAD 📥"
-DOWNLOAD_TEXT_URL = "https://omegalinks.in/ref/GreyMatter6458"
+DOWNLOAD_TEXT_URL = "https://t.me/VisionBackup/331"
 
    # Custom Caption Under Button #
 CAPTION_BUTTON = "Subscribe"
